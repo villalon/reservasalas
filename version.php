61f6bb9dd72c4f2de704d12f30638c85093a94a4
Xiufong <xiufong@192.168.0.10>
2015-03-21 20:25:15 -0300

    Cambio de idioma de los comentarios y se agregan los langs faltantes.

--- version.php
+++ version.php
@@ -8,26 +8,21 @@
 //
 // Moodle is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
+// along with Moodle. If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * 
  *
- * @package    local
+ * @package local
  * @subpackage reservasalas
- * @copyright  2014 Francisco García Ralph (francisco.garcia.ralph@gmail.com)
- * 					Nicolás Bañados Valladares (nbanados@alumnos.uai.cl)
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @copyright 2014 Francisco García Ralph (francisco.garcia.ralph@gmail.com)
+ *            Nicolás Bañados Valladares (nbanados@alumnos.uai.cl)
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-//Pertenece al plugin Reserva Salas
- 
- 
-defined('MOODLE_INTERNAL') || die();
+defined ( 'MOODLE_INTERNAL' ) || die ();
 $plugin->pluginname = "local_reservasalas";
 $plugin->component = 'local_reservasalas';
-$plugin->version   = '2014093010';
+$plugin->version = '2015032100';
